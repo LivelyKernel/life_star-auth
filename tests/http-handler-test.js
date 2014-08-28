8d411da021a8282bdbc2c8950fa945a2ba3712be
Robert Krahn <robert.krahn@gmail.com>
2014-08-28 01:51:31 +0000

    configure auth settings

--- tests/http-handler-test.js
+++ tests/http-handler-test.js
@@ -1,4 +1,4 @@
-/*global module, console, setTimeout*/
+/*global module, console, setTimeout, __dirname*/
 
 var path = require("path"),
     fs   = require('fs');
@@ -14,23 +14,55 @@ var path = require("path"),
 var testHelper   = require('life_star/tests/test-helper'),
     lifeStarTest = require("life_star/tests/life_star-test-support"),
     async        = require('async'),
-    testSuite    = {};
+    testSuite    = {},
+    serverConf   = {
+      authConf: {
+        enabled: true,
+        cookieField: "test-auth-cookie",
+        usersFile: path.join(__dirname, "test-users.json"),
+        paths: {
+          login: '/test-login',
+          register: '/test-register',
+          logout: '/test-logout'
+        }
+      },
+      fsNode: path.join(__dirname, "test-dir")
+    };
 
 testSuite.AuthHandlerTest = {
 
-  tearDown: function(run) {
-    lifeStarTest.shutDownLifeStar(run);
+  setUp: function(run) {
+    lifeStarTest.createDirStructure(__dirname, {
+      "test-dir": {"bar.js": "content 123", "foo.html": "<h1>hello world</h1>"}});
+    run();
   },
 
-  "unauthorized access refirects to login page": function(test) {
+  tearDown: function(run) {
+    lifeStarTest.cleanupTempFiles(function() {
+      lifeStarTest.shutDownLifeStar(run);
+    });
+  },
+
+  "unauthorized access redirects to login page": function(test) {
     lifeStarTest.withLifeStarDo(test, function() {
-      lifeStarTest.GET('/blank.html', function(res) {
+      lifeStarTest.GET('/foo.html', function(res) {
         test.equals(302, res.statusCode);
-        test.equals('Moved Temporarily. Redirecting to /uvic-login?redirect=%252Fblank.html', res.body);
+        test.equals('Moved Temporarily. Redirecting to /test-login?redirect=%252Ffoo.html', res.body);
         test.done();
       });
-    });
-  }
+    }, serverConf);
+  },
+
+  // "do login": function(test) {
+  //   lifeStarTest.withLifeStarDo(test, function() {
+  //     lifeStarTest.POST('/foo.html', function(res) {
+  //       test.equals(302, res.statusCode);
+  //       test.equals('Moved Temporarily. Redirecting to /test-login?redirect=%252Ffoo.html', res.body);
+  //       test.done();
+  //     });
+  //   }, {authConf: {authenticationEnabled: true, cookieField: "test-auth-cookie"},
+  //       fsNode: path.join(__dirname, "test-dir")});
+  // }
 
 }
 
